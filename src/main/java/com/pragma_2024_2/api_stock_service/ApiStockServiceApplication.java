033c96d6fc2e6ef3b3a485f70fd2a03d35d3a582
douglasd11 <dguerreroe@unicartagena.edu.co>
2024-09-02 20:45:21 -0500

    primer commit

---
+++ src/main/java/com/pragma_2024_2/api_stock_service/ApiStockServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pragma_2024_2.api_stock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStockServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStockServiceApplication.class, args);
+	}
+
+}
